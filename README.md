660f6a4700cc601deb44755b73de0d1a63a0aa35
Rene Feige <147383597+ReneFeige@users.noreply.github.com>
2023-11-06 09:53:05 +0100

    Added UI

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Simple rock, paper, scissors game, played in the console. Maybe add GUI later.
+
+Update:
+
+Added UI and it's now possible to play this game using inserted buttons. Once 5 rounds have been played, a winner is determined (player or computer). To start a new game simply click on Try Again. The text and text color changes depending on whether you have won, lost or tied.
--- script.js
+++ script.js
@@ -1,61 +1,124 @@
-function getComputerChoice() {
-    const options = ["Rock", "Paper", "Scissors"];
-    const randomChoice = Math.floor(Math.random() * options.length);
-    return options[randomChoice];
-}
-  
+const rockBtn = document.querySelector("#Rock");
+const paperBtn = document.querySelector("#Paper");
+const scissorsBtn = document.querySelector("#Scissors");
+const newText = document.querySelector("#changeText");
+const winner = document.querySelector("#whoHasWon");
+
+let playerSelection = null;
+let roundsPlayed = 0;
+let playerScore = 0;
+let computerScore = 0;
+let gameEnded = false;
+
 function game() {
-    let roundsPlayed = 0;
-    let playerScore = 0;
-    let computerScore = 0;
+    if (gameEnded) result;
 
-    for (i = 0; i < 5; i++) {
+    function getComputerChoice() {
+        const options = ["Rock", "Paper", "Scissors"];
+        const randomChoice = Math.floor(Math.random() * options.length);
+        return options[randomChoice];
+    }
+    
+    const computerSelection = getComputerChoice();
 
-        roundsPlayed++
-        const playerSelection = prompt("Rock|Paper|Scissors \nMake a choice");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        
-
-        function playRound(playerSelection, computerSelection) {
-            playerSelection = playerSelection.toLowerCase();
-            computerSelection = computerSelection.toLowerCase();
-
-            if (playerSelection === computerSelection) {
-                return ("It's a tie! You both picked " + playerSelection);
-            } else if (playerSelection === "rock" && computerSelection === "scissors") {
-                playerScore++
-                return ("You win! Rock beats scissors");
-            } else if (playerSelection === "rock" && computerSelection === "paper") {
-                computerScore++
-                return ("You lose! Paper beats rock");
-            } else if (playerSelection === "paper" && computerSelection === "rock") {
-                playerScore++
-                return ("You win! Paper beats rock");
-            } else if (playerSelection === "paper" && computerSelection === "scissors") {
-                computerScore++
-                return ("You lose! Scissors beat paper");
-            } else if (playerSelection === "scissors" && computerSelection === "paper") {
-                playerScore++
-                return ("You win! Scissors beat paper");
-            } else if (playerSelection === "scissors" && computerSelection === "rock") {
-                computerScore++
-                return ("You lose! Rock beats scissors");
-            }
+    function playRound() {
+        if (playerSelection === computerSelection) {
+            newText.style.color = "blue";
+            return ("It's a tie! You both picked " + playerSelection);
+        } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+            playerScore++
+            newText.style.color = "green";
+            return ("You win! Rock beats scissors");
+        } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+            computerScore++
+            newText.style.color = "red";
+            return ("You lose! Paper beats rock");
+        } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+            playerScore++
+            newText.style.color = "green";
+            return ("You win! Paper beats rock");
+        } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+            computerScore++
+            newText.style.color = "red";
+            return ("You lose! Scissors beats paper");
+        } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            playerScore++
+            newText.style.color = "green";
+            return ("You win! Scissors beats paper");
+        } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+            computerScore++
+            newText.style.color = "red";
+            return ("You lose! Rock beats scissors");
         }
     }
 
-    function win() {
-        if (playerScore === computerScore) {
-            return ("You tied with the Computer. Try Again!");
-        } else if (playerScore > computerScore) {
-            return ("Winner! You beat the Computer!");
-        } else if (playerScore < computerScore){
-            return ("Loser! You lost to the Computer");
-        }
-    }
+    newText.textContent = playRound();
 
-console.log("\nAfter " + roundsPlayed + " rounds: " + win());
+    if (roundsPlayed >= 5) {
+        winner.textContent = win();
+        gameEnded = true;
+        removeEventListeners();
+    }
 }
 
-game();
+function removeEventListeners() {
+    rockBtn.removeEventListener("click", rockButtonClick);
+    paperBtn.removeEventListener("click", paperButtonClick);
+    scissorsBtn.removeEventListener("click", scissorsButtonClick);
+}
+
+function win() {
+    if (playerScore === computerScore) {
+        winner.style.color = "blue";
+        return ("You tied with the Computer. Try Again!");
+    } else if (playerScore > computerScore) {
+        winner.style.color = "green";
+        return ("Winner! You beat the Computer! Play Again!");
+    } else if (playerScore < computerScore){
+        winner.style.color = "red";
+        return ("You lost to the Computer. Try Again!");
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    roundsPlayed = 0;
+    gameEnded = false;
+
+    winner.style.color = "black";
+    winner.textContent = "Rock Paper Scissors Game!";
+    newText.style.color = "black";
+    newText.textContent = "Make your choice!";
+
+    rockBtn.addEventListener("click", rockButtonClick);
+    paperBtn.addEventListener("click", paperButtonClick);
+    scissorsBtn.addEventListener("click", scissorsButtonClick);
+}
+
+
+function rockButtonClick() {
+    playerSelection = "Rock";
+    roundsPlayed++;
+    game();
+}
+
+function paperButtonClick() {
+    playerSelection = "Paper";
+    roundsPlayed++;
+    game();
+}
+
+function scissorsButtonClick() {
+    playerSelection = "Scissors";
+    roundsPlayed++;
+    game();
+}
+
+rockBtn.addEventListener("click", rockButtonClick);
+paperBtn.addEventListener("click", paperButtonClick);
+scissorsBtn.addEventListener("click", scissorsButtonClick);
+
+winner.addEventListener("click", () => {
+    resetGame();
+});
